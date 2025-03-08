be905c9431853f849f8c710ac819f02a7a7b9d40
tf7software <rhenrywarren@gmail.com>
2025-03-07 21:20:42 -0700

    Add homepage route and initial HTML file for user guidance

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const axios = require('axios');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 app.get('/nos', async (req, res) => {
   
   const xmlUrl = req.query.ifr;
